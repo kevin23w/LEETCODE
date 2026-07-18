4a246c398f1dc852bdb7e95f4121d403ac38a3f8
Kevin W <kevin2025wv@gmail.com>
2026-07-18 11:00:37 +0530

    Time: 7 ms (4.04%), Space: 20.5 MB (75.24%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,17 +1,12 @@
 class Solution:
     def search(self, arr: list[int], target: int) -> int:
-        low, high = 0, len(arr) - 1
-        
-        while low <= high:
-            # Bitwise right shift is highly optimized
-            mid = (low + high) >> 1
-            val = arr[mid]
-            
-            if val == target:
-                return mid
-            elif val < target:
-                low = mid + 1
+        l , r = 0 , len(arr) -1
+        while l <= r:
+            m = l + (r - l) // 2
+            if arr[m] > target:
+                r -= 1
+            elif arr[m] < target:
+                l += 1
             else:
-                high = mid - 1
-                
+                return m
         return -1
