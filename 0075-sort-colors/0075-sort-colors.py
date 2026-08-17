e798f7cd5574b288360379910ab97c8cddcf266a
Kevin W <kevin2025wv@gmail.com>
2026-08-17 11:17:28 +0530

    Time: 0 ms (100%), Space: 19.2 MB (61.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,15 +3,14 @@ class Solution:
         low = 0
         mid = 0
         high = len(nums) - 1
-        
-        # The 'while' statement must line up perfectly with 'low' above it
+
         while mid <= high:
             if nums[mid] == 0:
-                nums[low], nums[mid] = nums[mid], nums[low]
+                nums[low] , nums[mid] = nums[mid] , nums[low]
                 low += 1
                 mid += 1
             elif nums[mid] == 1:
                 mid += 1
             else:
-                nums[mid], nums[high] = nums[high], nums[mid]
+                nums[mid] , nums[high] = nums[high] , nums[mid]
                 high -= 1
